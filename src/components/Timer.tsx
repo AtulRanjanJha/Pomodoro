74e88014a6d7527041ca171ec4403fbb5b57bd0a
Atul Ranjan jha <99534159+AtulRanjanJha@users.noreply.github.com>
2025-07-13 00:06:26 +0530

    Update Timer.tsx

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -112,9 +112,9 @@ const Timer = ({ mode }: TimerProps) => {
 
   const handleReset = () => {
     const initial = {
-      pomodoro: timerDurations.pomodoro * 60,
-      shortBreak: timerDurations.shortBreak * 60,
-      longBreak: timerDurations.longBreak * 60,
+      pomodoro: timerDurations.pomodoro * 180,
+      shortBreak: timerDurations.shortBreak * 180,
+      longBreak: timerDurations.longBreak * 190,
     };
 
     setTime(initial[mode]);
